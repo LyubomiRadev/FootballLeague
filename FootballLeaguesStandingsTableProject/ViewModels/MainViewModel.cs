eec3317de23b47cf632e3e0d0d18daed6dc728c0
LyubomiRadev <lyubomiradev@gmail.com>
2023-09-07 13:05:58 +0300

    Added AsyncImageLoader. The combobox now shows each leagues logo.

--- FootballLeaguesStandingsTableProject/ViewModels/MainViewModel.cs
+++ FootballLeaguesStandingsTableProject/ViewModels/MainViewModel.cs
@@ -3,9 +3,6 @@
 using FootballLeaguesStandingsTableProject.Models;
 
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-
-using RestSharp;
 
 using System.Collections.ObjectModel;
 using System.ComponentModel;
@@ -17,8 +14,8 @@ namespace FootballLeaguesStandingsTableProject.ViewModels;
 public partial class MainViewModel : ViewModelBase
 {
 
-    public MainViewModel()
-    {
+	public MainViewModel()
+	{
 		this.LoadLeagues();
 	}
 
@@ -44,11 +41,6 @@ public partial class MainViewModel : ViewModelBase
 		}
 	}
 
-    private void RaiseAndSetProp(ref IdNameModel prop)
-	{
-
-	}
-
 	protected override void OnPropertyChanged(PropertyChangedEventArgs e)
 	{
 		base.OnPropertyChanged(e);
@@ -59,42 +51,57 @@ public partial class MainViewModel : ViewModelBase
 		}
 	}
 
+	#region Get API Data
+
 	private ObservableCollection<FootballCountryModel> GetApiData()
 	{
-		#region Api Call 
+		#region API Call 
 
-		var client = new RestClient("https://v3.football.api-sports.io/{endpoint}");
-		var request = new RestRequest("https://v3.football.api-sports.io/leagues", Method.Get) { RequestFormat = DataFormat.Json };
-		request.AddHeader("x-rapidapi-key", "5030880d82b1e6f3ee3612cb64c53569");
-		request.AddHeader("x-rapidapi-host", "v3.football.api-sports.io");
-		RestResponse response = client.Execute(request);
+		////setting the API
+		//var client = new RestClient("https://v3.football.api-sports.io/{endpoint}");
+		//var request = new RestRequest("https://v3.football.api-sports.io/leagues", Method.Get) { RequestFormat = DataFormat.Json };
+
+		//request.AddHeader("x-rapidapi-key", "5030880d82b1e6f3ee3612cb64c53569");
+		//request.AddHeader("x-rapidapi-host", "v3.football.api-sports.io");
+
+		////call the API
+		//RestResponse response = client.Execute(request);
+
 		var leaguesList = new ObservableCollection<FootballCountryModel>();
 
-		if (response.StatusCode == System.Net.HttpStatusCode.OK)
-		{
-			leaguesList = new ObservableCollection<FootballCountryModel>(JObject.Parse(response.Content)["response"].Select(x => x["league"].ToObject<FootballCountryModel>()).ToList());
-		}
+		////if the API call is good parse the date from the API and place it in the returnig collection
+		//if (response.StatusCode == System.Net.HttpStatusCode.OK)
+		//{
+		//	leaguesList = new ObservableCollection<FootballCountryModel>(JObject.Parse(response.Content)["response"].Select(x => x["league"].ToObject<FootballCountryModel>()).ToList());
+		//}
 
 		#endregion End Api Call
 
-		////path for Desktop PC => C:\Users\beckh\source\repos\FootballLeague\JSonLeagues.json
-		////path for Laptop => C:\Users\Lyubomir\Source\Repos\FootballLeague\JSonLeagues.json
-		//var rawLeaguesDataJSON = JsonConvert.DeserializeObject<LeaguesDeserializedModel>(File.ReadAllText(@"C:\Users\beckh\source\repos\FootballLeague\JSonLeagues.json"));
-		//var leaguesList = new ObservableCollection<FootballCountryModel>();
+		#region Depricated JSON PARSING
 
-		//foreach (var rawLeagueData in rawLeaguesDataJSON.Response)
-		//{
-		//	var lg = JsonConvert.DeserializeObject<Results>(rawLeagueData.ToString());
-		//	var leagueData = JsonConvert.DeserializeObject<FootballCountryModel>(lg.JsonResults.ToString());
+		//path for Desktop PC => C:\Users\beckh\source\repos\FootballLeague\JSonLeagues.json
+		//path for Laptop => C:\Users\Lyubomir\Source\Repos\FootballLeague\JSonLeagues.json
+		var rawLeaguesDataJSON = JsonConvert.DeserializeObject<LeaguesDeserializedModel>(File.ReadAllText(@"C:\Users\beckh\source\repos\FootballLeague\JSonLeagues.json"));
+		leaguesList = new ObservableCollection<FootballCountryModel>();
 
-		//	if (leagueData != null)
-		//	{
-		//		leaguesList.Add(leagueData);
-		//	}
-		//}
+		foreach (var rawLeagueData in rawLeaguesDataJSON.Response)
+		{
+			//leaguesList = new ObservableCollection<FootballCountryModel>(JObject.Parse(response.Content)["response"].Select(x => x["league"].ToObject<FootballCountryModel>()).ToList());
+			var lg = JsonConvert.DeserializeObject<Results>(rawLeagueData.ToString());
+			var leagueData = JsonConvert.DeserializeObject<FootballCountryModel>(lg.JsonResults.ToString());
+
+			if (leagueData != null)
+			{
+				leaguesList.Add(leagueData);
+			}
+		}
+
+		#endregion End Depricated JSON PARSING
 
 		return leaguesList;
 	}
+
+	#endregion End Get API Data
 
 	#endregion
 
